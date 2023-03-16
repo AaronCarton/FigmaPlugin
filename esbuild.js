0bfc0d72360538ad090cecb2af547ba64ee1f4dd
Jules Victor <victorj@delawareconsulting.com>
2023-03-16 15:27:18 +0100

    formatting

--- esbuild.js
+++ esbuild.js
@@ -1,49 +1,52 @@
-const esbuild = require('esbuild');
+const esbuild = require("esbuild")
 esbuild
   .build({
-    entryPoints: ['src/code.ts'],
+    entryPoints: ["src/code.ts"],
     bundle: true,
-    platform: 'node',
-    target: ['node10.4'],
-    outfile: 'dist/code.js'
+    platform: "node",
+    target: ["node10.4"],
+    outfile: "dist/code.js",
   })
-  .catch(() => process.exit(1));
-  const {readFile, writeFile} = require('fs').promises;
-  const minify = require('html-minifier-terser').minify;
+  .catch(() => process.exit(1))
+const { readFile, writeFile } = require("fs").promises
+const minify = require("html-minifier-terser").minify
 // iframe UI
 
-(async () => {
-    const script = esbuild.buildSync({
-      entryPoints: ['src/ui.ts'],
-      bundle: true,
-      minify: true,
-      write: false,
-      target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-    });
-    const css = esbuild.buildSync({
-        entryPoints: ['src/style.css'],
-        bundle: true,
-        minify: true,
-        write: false,
-        target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-      });
-  
-    const html = await readFile('src/ui.html', 'utf8');
-    // const css = await readFile('src/style.css', 'utf8');
+;(async () => {
+  const script = esbuild.buildSync({
+    entryPoints: ["src/ui.ts"],
+    bundle: true,
+    minify: true,
+    write: false,
+    target: ["chrome58", "firefox57", "safari11", "edge16"],
+  })
+  const css = esbuild.buildSync({
+    entryPoints: ["src/style.css"],
+    bundle: true,
+    minify: true,
+    write: false,
+    target: ["chrome58", "firefox57", "safari11", "edge16"],
+  })
 
-    const minifyOptions = {
-      collapseWhitespace: true,
-      keepClosingSlash: true,
-      removeComments: true,
-      removeRedundantAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      useShortDoctype: true,
-      minifyCSS: true
-    };
-  
-    await writeFile(
-      'dist/ui.html',
-      `<script>${script.outputFiles[0].text}</script><style>${await minify(css.outputFiles[0].text, minifyOptions)}</style>${await minify(html, minifyOptions)}`
-    );
-  })();
+  const html = await readFile("src/ui.html", "utf8")
+  // const css = await readFile('src/style.css', 'utf8');
+
+  const minifyOptions = {
+    collapseWhitespace: true,
+    keepClosingSlash: true,
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    useShortDoctype: true,
+    minifyCSS: true,
+  }
+
+  await writeFile(
+    "dist/ui.html",
+    `<script>${script.outputFiles[0].text}</script><style>${await minify(
+      css.outputFiles[0].text,
+      minifyOptions,
+    )}</style>${await minify(html, minifyOptions)}`,
+  )
+})()
--- eswatch.js
+++ eswatch.js
@@ -1,31 +1,31 @@
-const esbuild = require('esbuild');
-const watch = require('node-watch');
-const build = ()=>{
-    esbuild
-  .build({
-    entryPoints: ['src/code.ts'],
-    bundle: true,
-    platform: 'node',
-    target: ['node10.4'],
-    outfile: 'dist/code.js'
-  })
-  .catch(() => process.exit(1));
-  
-  const {readFile, writeFile} = require('fs').promises;
-  const minify = require('html-minifier-terser').minify;
-// iframe UI
+const esbuild = require("esbuild")
+const watch = require("node-watch")
+const build = () => {
+  esbuild
+    .build({
+      entryPoints: ["src/code.ts"],
+      bundle: true,
+      platform: "node",
+      target: ["node10.4"],
+      outfile: "dist/code.js",
+    })
+    .catch(() => process.exit(1))
 
-(async () => {
+  const { readFile, writeFile } = require("fs").promises
+  const minify = require("html-minifier-terser").minify
+  // iframe UI
+
+  ;(async () => {
     const script = esbuild.buildSync({
-      entryPoints: ['src/ui.ts'],
+      entryPoints: ["src/ui.ts"],
       bundle: true,
       minify: true,
       write: false,
-      target: ['chrome58', 'firefox57', 'safari11', 'edge16']
-    });
-  
-    const html = await readFile('src/ui.html', 'utf8');
-    const css = await readFile('src/style.css', 'utf8');
+      target: ["chrome58", "firefox57", "safari11", "edge16"],
+    })
+
+    const html = await readFile("src/ui.html", "utf8")
+    const css = await readFile("src/style.css", "utf8")
 
     const minifyOptions = {
       collapseWhitespace: true,
@@ -35,15 +35,17 @@ const build = ()=>{
       removeScriptTypeAttributes: true,
       removeStyleLinkTypeAttributes: true,
       useShortDoctype: true,
-      minifyCSS: true
-    };
-  
+      minifyCSS: true,
+    }
+
     await writeFile(
-      'dist/ui.html',
-      `<script>${script.outputFiles[0].text}</script><style>${await minify(css, minifyOptions)}</style>${await minify(html, minifyOptions)}`
-    );
-  })();
+      "dist/ui.html",
+      `<script>${script.outputFiles[0].text}</script><style>${await minify(
+        css,
+        minifyOptions,
+      )}</style>${await minify(html, minifyOptions)}`,
+    )
+  })()
 }
 
-watch('./src', {recursive: true}, build);
-
+watch("./src", { recursive: true }, build)
