c9e51563e361e29a11e75f3c4d71c2d8f0cfb765
LARS SOENS <lars.soens@delaware.pro>
2023-03-17 11:32:43 +0100

    Fixed issues from PR

--- src/ui/navigation-tabs.ts
+++ src/ui/navigation-tabs.ts
@@ -1,39 +1,51 @@
-const connect = document.querySelector(".js-connect")
-const settings = document.querySelector(".js-settings")
-const usage = document.querySelector(".js-usage")
+const connect: HTMLElement = document.querySelector(".js-connect")!
+const settings: HTMLElement = document.querySelector(".js-settings")!
+const usage: HTMLElement = document.querySelector(".js-usage")!
 
-const connectView = document.querySelector(".js-connect-view")
-const settingsView = document.querySelector(".js-settings-view")
-const usageView = document.querySelector(".js-usage-view")
+const connectView: HTMLElement = document.querySelector(".js-connect-view")!
+const settingsView: HTMLElement = document.querySelector(".js-settings-view")!
+const usageView: HTMLElement = document.querySelector(".js-usage-view")!
 
-const settingsIcon = document.querySelector(".js-settings-icon")
+const navItems: NodeListOf<HTMLElement> = document.querySelectorAll(".js-nav-item")
+const panelItems: NodeListOf<HTMLElement> = document.querySelectorAll(".js-panel-item")
 
-connect?.addEventListener("click", function () {
-  connect?.classList.add("selected")
-  settings?.classList.remove("selected")
-  usage?.classList.remove("selected")
-  connectView?.classList.remove("close")
-  settingsView?.classList.add("close")
-  usageView?.classList.add("close")
-  settingsIcon?.classList.remove("selected")
+const settingsIcon: HTMLElement = document.querySelector(".js-settings-icon")!
+
+const isSelected: string = "is-selected"
+const isActive: string = "is-active"
+
+connect.addEventListener("click", function () {
+  navItems.forEach((item) => {
+    item.classList.remove(isSelected)
+  })
+  panelItems.forEach((item) => {
+    item.classList.remove(isActive)
+  })
+  connect.classList.add(isSelected)
+  connectView.classList.add(isActive)
+  settingsIcon.classList.remove(isSelected)
 })
 
-settings?.addEventListener("click", function () {
-  settings?.classList.add("selected")
-  connect?.classList.remove("selected")
-  usage?.classList.remove("selected")
-  settingsView?.classList.remove("close")
-  connectView?.classList.add("close")
-  usageView?.classList.add("close")
-  settingsIcon?.classList.add("selected")
+settings.addEventListener("click", function () {
+  navItems.forEach((item) => {
+    item.classList.remove(isSelected)
+  })
+  panelItems.forEach((item) => {
+    item.classList.remove(isActive)
+  })
+  settings.classList.add(isSelected)
+  settingsView.classList.add(isActive)
+  settingsIcon.classList.add(isSelected)
 })
 
-usage?.addEventListener("click", function () {
-  usage?.classList.add("selected")
-  connect?.classList.remove("selected")
-  settings?.classList.remove("selected")
-  usageView?.classList.remove("close")
-  connectView?.classList.add("close")
-  settingsView?.classList.add("close")
-  settingsIcon?.classList.remove("selected")
+usage.addEventListener("click", function () {
+  navItems.forEach((item) => {
+    item.classList.remove(isSelected)
+  })
+  panelItems.forEach((item) => {
+    item.classList.remove(isActive)
+  })
+  usage.classList.add(isSelected)
+  usageView.classList.add(isActive)
+  settingsIcon.classList.remove(isSelected)
 })
