d760389b7b8d5ca46e374792f459a837e0804659
Jules Victor <victorj@delawareconsulting.com>
2023-03-20 14:57:50 +0100

    del comments

--- src/ui/settings.ts
+++ src/ui/settings.ts
@@ -7,12 +7,10 @@ const $button: HTMLButtonElement | null = document.querySelector(".c-plugin__btn
 const $spinner: HTMLElement | null = document.querySelector(".c-plugin__loader");
 const $plugin: HTMLElement | null = document.querySelector(".js-settings-view");
 const checkConnection = async () => {
-  //adding spinner + adding no pointer class
   $plugin?.classList.add("no-pointer");
   $spinner?.removeAttribute("hidden");
   const dbURL: string | null | undefined = $dbURL?.value.replace(/\s/g, "").trim();
   const apiKey: string | null | undefined = $apiKey?.value.replace(/\s/g, "").trim();
-  console.log(dbURL, apiKey);
   fetch("https://www.mocky.io/v2/5185415ba171ea3a00704eed?mocky-delay=5000ms")
     .then((response) => response.json())
     .then(() => {
@@ -33,7 +31,6 @@ const toggleAnnotations = (e: Event) => {
 const disableFields = () => {
   if ($dbURL !== null && $apiKey !== null && $annotationToggle !== null && $button !== null) {
     $annotationToggle.disabled = true;
-    //replace makes sure people can not connect with empty strings (for example pressing spacebar)
     if ($dbURL.value.replace(/\s/g, "") !== "") {
       $apiKey.disabled = false;
     } else {
