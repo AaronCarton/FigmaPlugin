1140af11b604c974826af5fc646f5697b0ce47b6
Jules Victor <victorj@delawareconsulting.com>
2023-03-20 10:44:05 +0100

    disable connect fields based on input

--- src/ui/settings.ts
+++ src/ui/settings.ts
@@ -1,6 +1,10 @@
+//input elements
+const $dbURL: HTMLInputElement | null = document.querySelector("#settings_dbLink")
+const $apiKey: HTMLInputElement | null = document.querySelector("#settings_apiKey")
+const $annotationToggle: HTMLInputElement | null = document.querySelector("#annotationToggle")
+const $button: HTMLButtonElement | null = document.querySelector(".c-plugin__btnConnect")
+
 const checkConnection = async () => {
-  const $dbURL: HTMLInputElement | null = document.querySelector("#settings_dbLink")
-  const $apiKey: HTMLInputElement | null = document.querySelector("#settings_apiKey")
   const dbURL: string | null | undefined = $dbURL?.value
   const apiKey: string | null | undefined = $apiKey?.value
   console.log(dbURL, apiKey)
@@ -15,11 +19,30 @@ const toggleAnnotations = (e: Event) => {
   }
 }
 
+const disableFields = () => {
+  console.log($dbURL?.value)
+  if ($dbURL !== null && $apiKey !== null && $annotationToggle !== null && $button !== null) {
+    $annotationToggle.disabled = true
+    if ($dbURL.value !== "") {
+      $apiKey.disabled = false
+    } else {
+      $apiKey.disabled = true
+    }
+    if ($dbURL.value === "" || $apiKey.value === "") {
+      $button.removeEventListener("click", checkConnection)
+    }
+    if ($dbURL.value !== "" && $apiKey.value !== "") {
+      $annotationToggle.disabled = false
+      $button.addEventListener("click", checkConnection)
+    }
+  }
+}
+
 const init = () => {
-  const $button: HTMLButtonElement | null = document.querySelector(".c-plugin__btnConnect")
-  $button?.addEventListener("click", checkConnection)
-  const $annotationToggle: HTMLInputElement | null = document.querySelector("#annotationToggle")
+  disableFields()
   $annotationToggle?.addEventListener("click", toggleAnnotations)
+  $dbURL?.addEventListener("keyup", disableFields)
+  $apiKey?.addEventListener("keyup", disableFields)
 }
 
 init()
