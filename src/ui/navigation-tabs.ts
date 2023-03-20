6a9fa4fd46ca0f03b971ccb68ba906882871503d
LARS SOENS <lars.soens@delaware.pro>
2023-03-20 14:05:20 +0100

    PR fixes

--- src/ui/navigation-tabs.ts
+++ src/ui/navigation-tabs.ts
@@ -1,51 +1,25 @@
-const connect: HTMLElement = document.querySelector(".js-connect")!;
-const settings: HTMLElement = document.querySelector(".js-settings")!;
-const usage: HTMLElement = document.querySelector(".js-usage")!;
-
-const connectView: HTMLElement = document.querySelector(".js-connect-view")!;
-const settingsView: HTMLElement = document.querySelector(".js-settings-view")!;
-const usageView: HTMLElement = document.querySelector(".js-usage-view")!;
-
-const navItems: NodeListOf<HTMLElement> = document.querySelectorAll(".js-nav-item");
+const tabs: NodeListOf<HTMLElement> = document.querySelectorAll(".js-tab");
 const panelItems: NodeListOf<HTMLElement> = document.querySelectorAll(".js-panel-item");
 
 const settingsIcon: HTMLElement = document.querySelector(".js-settings-icon")!;
 
-const isSelected = "is-selected";
 const isActive = "is-active";
 
-connect.addEventListener("click", function () {
-  navItems.forEach((item) => {
-    item.classList.remove(isSelected);
-  });
-  panelItems.forEach((item) => {
-    item.classList.remove(isActive);
-  });
-  connect.classList.add(isSelected);
-  connectView.classList.add(isActive);
-  settingsIcon.classList.remove(isSelected);
-});
-
-settings.addEventListener("click", function () {
-  navItems.forEach((item) => {
-    item.classList.remove(isSelected);
-  });
-  panelItems.forEach((item) => {
-    item.classList.remove(isActive);
-  });
-  settings.classList.add(isSelected);
-  settingsView.classList.add(isActive);
-  settingsIcon.classList.add(isSelected);
-});
-
-usage.addEventListener("click", function () {
-  navItems.forEach((item) => {
-    item.classList.remove(isSelected);
-  });
-  panelItems.forEach((item) => {
-    item.classList.remove(isActive);
-  });
-  usage.classList.add(isSelected);
-  usageView.classList.add(isActive);
-  settingsIcon.classList.remove(isSelected);
-});
+tabs.forEach((trigger) =>
+  trigger.addEventListener("click", function () {
+    const selectedTab = trigger.getAttribute("data-target")!;
+    tabs.forEach((item) => {
+      item.classList.remove(isActive);
+    });
+    panelItems.forEach((item) => {
+      item.classList.remove(isActive);
+    });
+    trigger.classList.add(isActive);
+    document.getElementById(selectedTab)?.classList.add(isActive);
+    if (selectedTab === "settings") {
+      settingsIcon.classList.add(isActive);
+    } else {
+      settingsIcon.classList.remove(isActive);
+    }
+  }),
+);
