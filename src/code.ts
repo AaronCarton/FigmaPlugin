23fc649a4ecd3fc820c6050063b130616a445657
LARS SOENS <lars.soens@delaware.pro>
2023-03-21 09:48:22 +0100

    Added connection check

--- src/code.ts
+++ src/code.ts
@@ -3,13 +3,28 @@ figma.showUI(__html__, { width: 345, height: 250 });
 figma.ui.onmessage = (event) => {
   const eventType = event.type;
   const selectedTab = event.tab;
+  const connectionState = event.connection;
+
   if (eventType == "changeTab") {
     if (selectedTab == "connect") {
-      figma.ui.resize(345, 250);
+      if (connectionState) {
+        figma.ui.resize(345, 250);
+      } else {
+        figma.ui.resize(345, 124);
+      }
+      console.log(connectionState);
     } else if (selectedTab == "settings") {
       figma.ui.resize(345, 235);
     } else if (selectedTab == "usage") {
       figma.ui.resize(345, 586);
+    }
+  }
+
+  if (eventType == "connectionCheck") {
+    if (connectionState) {
+      figma.ui.resize(345, 250);
+    } else {
+      figma.ui.resize(345, 124);
     }
   }
 };
--- src/ui/navigation-tabs.ts
+++ src/ui/navigation-tabs.ts
@@ -1,7 +1,27 @@
 const tabs: NodeListOf<HTMLElement> = document.querySelectorAll(".js-tab");
 const panelItems: NodeListOf<HTMLElement> = document.querySelectorAll(".js-panel-item");
 
+const connectionPanel: HTMLElement = document.querySelector(".js-connection")!;
+const noConnectionPanel: HTMLElement = document.querySelector(".js-no-connection")!;
+
 const isActive = "is-active";
+const connectionState = true;
+
+const checkConnection = () => {
+  if (connectionState) {
+    console.log("Connection is active");
+    connectionPanel.classList.add(isActive);
+    noConnectionPanel.classList.remove(isActive);
+  } else {
+    console.log("Connection is inactive");
+    connectionPanel.classList.remove(isActive);
+    noConnectionPanel.classList.add(isActive);
+  }
+  parent.postMessage(
+    { pluginMessage: { type: "connectionCheck", connection: connectionState } },
+    "*",
+  );
+};
 
 tabs.forEach((trigger) =>
   trigger.addEventListener("click", function () {
@@ -14,6 +34,11 @@ tabs.forEach((trigger) =>
     });
     trigger.classList.add(isActive);
     document.getElementById(selectedTab)?.classList.add(isActive);
-    parent.postMessage({ pluginMessage: { type: "changeTab", tab: selectedTab } }, "*");
+    parent.postMessage(
+      { pluginMessage: { type: "changeTab", tab: selectedTab, connection: connectionState } },
+      "*",
+    );
   }),
 );
+
+checkConnection();
