f2e6069caa71bd59d9aa16bd7b334a6a4a6e832b
Sebastien Vanlede <56797089+SebastienVanlede@users.noreply.github.com>
2023-03-22 10:53:27 +0100

    Temporary for merge.

--- src/services/events/events.ts
+++ src/services/events/events.ts
@@ -1,53 +1,43 @@
 import { RequestOptions } from "../api/client";
-import client from "../api/client";
+import ApiClient from "../api/client";
 import Project from "../../interfaces/interface.project";
 
 export default () => {
-  const api = client();
+  const api = ApiClient();
 
   async function makeConnection(baseURL: string, clientKey: string, sourceKey: string) {
+    checkIfProjectExist(baseURL, "test");
     const connection = await api.connect(baseURL, clientKey, sourceKey);
-    checkIfProjectExist(baseURL, "test", clientKey);
     console.log(connection);
-  }
-
-  async function getData(baseURL: string, options: RequestOptions<unknown>) {
-    const data = await api.getData(baseURL, options);
-    console.log(data);
   }
 
   // Function to check is the project already exists or not.
   // If project does not exist yet, a new project will be made with a projectKey, if it already exists the data will be returned.
-  async function checkIfProjectExist(baseURL: string, projectKey: string, clientKey: string) {
+  async function checkIfProjectExist(baseURL: string, projectKey: string) {
     // URL check to see if url is valid or not.
     validateUrl(baseURL);
 
-    if (await api.searchProjects(projectKey)) {
-      console.log("Project already exists");
-      const data = await getData(baseURL, {
-        method: "POST",
-        apiKey: clientKey,
-        body: {
-          query: {
-            bool: {
-              must: {
-                match: {
-                  itemKey: projectKey,
-                },
-              },
-            },
-          },
-        },
-      });
+    // Check if project exists
+    if (projectKey !== null || projectKey !== undefined) {
+      getProjectData(projectKey);
+      getAnnotationData(projectKey, false);
     } else {
-      console.log("Project does not exist, creating new project");
-      api.upsertProject({
-        itemType: "project",
-        partition: "null",
-        itemKey: projectKey,
-        localized: [],
-      } as Project);
+      console.error("Project key is not valid");
     }
+  }
+
+  // Function to get project data from ODS
+  async function getProjectData(projectKey: string) {
+    const data = await api.searchProjects(projectKey);
+    console.log(data);
+    return data;
+  }
+
+  // Function to get annotation data from ODS
+  async function getAnnotationData(projectKey: string, showDeleted: boolean){
+    const data = await api.searchAnnotations(projectKey, showDeleted);
+    console.log(data);
+    return data;
   }
 
   return {
