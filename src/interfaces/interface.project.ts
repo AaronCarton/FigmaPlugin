8206b5324f662071e010c077d568848bac872922
Aaron <cartona@delawareconsulting.com>
2023-03-24 15:04:50 +0100

    convert interfaces to classes
    with built-in functions

--- src/interfaces/interface.project.ts
+++ src/interfaces/interface.project.ts
@@ -1,8 +1,14 @@
-import { ODSobject } from "./ods/interface.ODSresponse";
+import ApiClient from "../services/api/client";
+import { ODSObject } from "./ods/interface.ODSresponse";
 
-export interface IProject {
-  lastUpdated: string;
-  customerId: string;
+export default class Project extends ODSObject<Project> {
+  public lastUpdated: string;
+  public customerId: string;
+
+  constructor(project: Project, api: ApiClient) {
+    super(api, project);
+
+    this.lastUpdated = project.lastUpdated;
+    this.customerId = project.customerId;
+  }
 }
-
-export default interface Project extends IProject, ODSobject {}
--- src/interfaces/ods/interface.ODSresponse.ts
+++ src/interfaces/ods/interface.ODSresponse.ts
@@ -1,13 +1,4 @@
-/**
- * ODSobject is a generic interface for the objects returned by the ODS API.
- * As all objects will include these properties.
- */
-export interface ODSobject {
-  itemType: string;
-  partition: string;
-  itemKey: string;
-  localized: [];
-}
+import ApiClient from "../../services/api/client";
 
 /**
  *  ODSresponse is a generic interface for the response from the ODS API.
@@ -15,7 +6,7 @@ export interface ODSobject {
  * @param U is the type of an optional parent object in the response (e.g. Project)
  * @param K is the key under which the parent will be found (e.g. "project")
  */
-export interface ODSresponse<T extends ODSobject, U = undefined, K extends string = ""> {
+export interface ODSResponse<T extends ODSObject<T>, U = undefined, K extends string = ""> {
   results: Array<
     {
       item: T;
@@ -25,4 +16,78 @@ export interface ODSresponse<T extends ODSobject, U = undefined, K extends strin
           [P in K]: U;
         })
   >;
+}
+
+/**
+ * ODSObject is an abstract generic class for the objects returned by the ODS API.
+ * As all objects will include these properties.
+ *
+ * It also includes some helper functions, such as saving and archiving.
+ *
+ * IT IS NOT MEANT TO BE INSTANTIATED DIRECTLY.
+ */
+export abstract class ODSObject<T extends ODSObject<T>> {
+  protected API: ApiClient;
+
+  public itemType: string;
+  public partition: string;
+  public itemKey: string;
+  public localized: [];
+  public deleted: boolean;
+
+  constructor(api: ApiClient, obj: T) {
+    this.API = api;
+
+    this.itemType = obj.itemType;
+    this.partition = obj.partition;
+    this.itemKey = obj.itemKey;
+    this.localized = obj.localized;
+    this.deleted = obj.deleted;
+  }
+
+  /**
+   * Archive the item
+   *
+   * sets the deleted property to true and calls save()
+   */
+  public async archive() {
+    this.deleted = true;
+    this.save();
+  }
+
+  /**
+   * Restore the item
+   *
+   * sets the deleted property to false and calls save()
+   */
+  public async restore() {
+    this.deleted = false;
+    this.save();
+  }
+
+  /**
+   * Save the item
+   */
+  public async save() {
+    await this.API.upsertItem(this.itemType, this.itemKey, this.stripODS());
+  }
+
+  /**
+   * stripODS() removes the ODSobject properties from the child object
+   * and returns a new object without them.
+   * This is necessary because the ODS API does not accept
+   * these additional properties when updating an object.
+   *
+   * @returns {T} a new object without the ODS properties
+   */
+  public stripODS(): T {
+    const x = { ...this } as Record<string, unknown>;
+    delete x.API;
+    delete x.itemKey;
+    delete x.itemType;
+    delete x.localized;
+    delete x.partition;
+
+    return x as T;
+  }
 }
