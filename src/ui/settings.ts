3d6562e4dec1b8545888a491c74d88c4afef81cb
Jules Victor <victorj@delawareconsulting.com>
2023-03-30 08:39:56 +0200

    eslint errors

--- src/ui/settings.ts
+++ src/ui/settings.ts
@@ -6,29 +6,30 @@ const $button: HTMLButtonElement | null = document.querySelector(".c-plugin__btn
 //Spinner
 const $spinner: HTMLElement | null = document.querySelector(".c-plugin__loader");
 const $plugin: HTMLElement | null = document.querySelector(".js-settings-view");
-const checkConnection = async () => {
+
+function checkConnection() {
   $plugin?.classList.add("no-pointer");
   $spinner?.removeAttribute("hidden");
-  const dbURL: string | null | undefined = $dbURL?.value.replace(/\s/g, "").trim();
-  const apiKey: string | null | undefined = $apiKey?.value.replace(/\s/g, "").trim();
+  // const dbURL: string | null | undefined = $dbURL?.value.replace(/\s/g, "").trim();
+  // const apiKey: string | null | undefined = $apiKey?.value.replace(/\s/g, "").trim();
   fetch("https://www.mocky.io/v2/5185415ba171ea3a00704eed?mocky-delay=5000ms")
     .then((response) => response.json())
     .then(() => {
       $plugin?.classList.remove("no-pointer");
       $spinner?.setAttribute("hidden", "");
     });
-};
+}
 
-const toggleAnnotations = (e: Event) => {
+function toggleAnnotations(e: Event) {
   const state: boolean = (<HTMLInputElement>e.target).checked;
   if (state === true) {
     console.log("show annotations");
   } else {
     console.log("hide annotations");
   }
-};
+}
 
-const disableFields = () => {
+function disableFields() {
   if ($dbURL !== null && $apiKey !== null && $annotationToggle !== null && $button !== null) {
     $annotationToggle.disabled = true;
     if ($dbURL.value.replace(/\s/g, "") !== "") {
@@ -44,13 +45,13 @@ const disableFields = () => {
       $button.addEventListener("click", checkConnection);
     }
   }
-};
+}
 
-const init = () => {
+function init() {
   disableFields();
   $annotationToggle?.addEventListener("click", toggleAnnotations);
   $dbURL?.addEventListener("keyup", disableFields);
   $apiKey?.addEventListener("keyup", disableFields);
-};
+}
 
 init();
