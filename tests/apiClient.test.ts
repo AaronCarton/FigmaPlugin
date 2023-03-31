f2fa85d7dadac1bd49d49e519608fc694e07d5fd
verhaegec <91324449+verhaegec@users.noreply.github.com>
2023-03-31 15:19:40 +0200

    Update apiClient.test.ts

--- tests/apiClient.test.ts
+++ tests/apiClient.test.ts
@@ -63,24 +63,24 @@ describe("Tests for API client", () => {
     });
   });
 
-  test("Can create archived project", async () => {
-    jest.setTimeout(30000);
-    const project = {
-      lastUpdated: new Date().toISOString(),
-      customerId: "896",
-    };
-    await apiClient.createProject("75", project).then(async (res) => {
-      res.archive();
-      expect(res.archived).not.toEqual("null");
-    });
-  });
+  // test("Can archived project", async () => {
+  //   jest.setTimeout(30000);
+  //   const project = {
+  //     lastUpdated: new Date().toISOString(),
+  //     customerId: "896",
+  //   };
+  //   await apiClient.createProject("74", project).then(async (res) => {
+  //     res.archive();
+  //     expect(res.archived).not.toEqual("null");
+  //   });
+  // });
 
   test("Get archived project (by projectKey) when archived = false", async () => {
     jest.setTimeout(10000);
-    const project = await apiClient.getProject("75", false).then(async (res) => {
+    const project = await apiClient.getProject("74", false).then(async (res) => {
       res[0].archive();
-      const archivedProject = await apiClient.getProject("75", false);
-      expect(archivedProject[0]).toBeNull();
+      const archivedProject = await apiClient.getProject("74", false);
+      expect(archivedProject).toEqual(0);
     });
   });
   // test("Get annotation by projectKey when archived", async () => {
