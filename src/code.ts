436d5a6c20e6fedf59474ec2fd825cd7096feacb
LARS SOENS <lars.soens@delaware.pro>
2023-03-31 13:35:33 +0200

    Added change event

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,66 @@
 class messageTitle {
   public static readonly changeTab: string = "changeTab";
   public static readonly connectionCheck: string = "connectionCheck";
+  public static readonly createText: string = "createText";
+}
+
+async function loadFonts() {
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+  await figma.loadFontAsync({ family: "Inter", style: "Semi Bold" });
+}
+
+function createAnnotation(inputValues: string[]) {
+  const page = figma.currentPage;
+  const frame = figma.createFrame();
+  frame.name = "Annotation";
+  frame.x = figma.viewport.center.x;
+  frame.y = figma.viewport.center.y;
+  frame.resize(320, 130);
+  page.appendChild(frame);
+
+  frame.fills = [{ type: "SOLID", color: { r: 0.9, g: 0.96, b: 1 } }];
+  frame.strokes = [{ type: "SOLID", color: { r: 0.05, g: 0.6, b: 1 } }];
+  frame.strokeWeight = 1;
+  frame.strokeAlign = "CENTER";
+  frame.strokeCap = "ROUND";
+  frame.strokeJoin = "ROUND";
+  frame.dashPattern = [10, 5];
+  frame.cornerRadius = 10;
+
+  const textTitles = ["Data source", "Entity", "Attribute", "Data type", "Sample value"];
+  const textValues = [
+    inputValues[0],
+    inputValues[1],
+    inputValues[2],
+    inputValues[3],
+    inputValues[4],
+  ];
+
+  for (let i = 0; i < 5; i++) {
+    const titlesNode = figma.createText();
+    titlesNode.name = textTitles[i];
+    titlesNode.characters = textTitles[i].toString();
+    titlesNode.fontSize = 11;
+    titlesNode.x = 15;
+    titlesNode.y = 22 * i + 15;
+    titlesNode.fontName = { family: "Inter", style: "Semi Bold" };
+    titlesNode.fills = [{ type: "SOLID", color: { r: 0.2, g: 0.2, b: 0.2 } }];
+    frame.appendChild(titlesNode);
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const valuesNode = figma.createText();
+    valuesNode.name = textValues[i];
+    valuesNode.characters = textValues[i].toString();
+    valuesNode.fontSize = 11;
+    valuesNode.x = 105;
+    valuesNode.y = 22 * i + 15;
+    valuesNode.fills = [{ type: "SOLID", color: { r: 0.2, g: 0.2, b: 0.2 } }];
+    frame.appendChild(valuesNode);
+  }
+
+  frame.locked = true;
+  frame.visible = true;
 }
 
 figma.showUI(__html__, { width: 345, height: 250 });
@@ -9,6 +69,8 @@ figma.ui.onmessage = (event) => {
   const eventType = event.type;
   const selectedTab = event.tab;
   const connectionState = event.connection;
+
+  loadFonts();
 
   if (eventType == messageTitle.changeTab) {
     switch (selectedTab) {
@@ -38,4 +100,17 @@ figma.ui.onmessage = (event) => {
       figma.ui.resize(345, 124);
     }
   }
+
+  if (eventType == messageTitle.createText) {
+    createAnnotation(event.values);
+  }
 };
+
+figma.on("close", async () => {
+  console.log("closing");
+  figma.currentPage.children.forEach((child) => {
+    if (child.name === "Annotation") {
+      child.remove();
+    }
+  });
+});
--- src/ui/connect-panel.ts
+++ src/ui/connect-panel.ts
@@ -1,17 +1,125 @@
-const buttons: NodeListOf<HTMLElement> = document.querySelectorAll(".js-btn");
+const buttons: NodeListOf<HTMLElement> | null = document.querySelectorAll(".js-btn");
+
+const dataSrc: HTMLInputElement | null = document.querySelector(".js-data-source");
+const dataSrcText: HTMLInputElement | null = document.querySelector(".js-data-source-text");
+const entity: HTMLInputElement | null = document.querySelector(".js-entity");
+const entityText: HTMLInputElement | null = document.querySelector(".js-entity-text");
+const attribute: HTMLInputElement | null = document.querySelector(".js-attribute");
+const attributeText: HTMLInputElement | null = document.querySelector(".js-attribute-text");
+const dataType: HTMLInputElement | null = document.querySelector(".js-data-type");
+const dataTypeText: HTMLInputElement | null = document.querySelector(".js-data-type-text");
+const sampleValue: HTMLInputElement | null = document.querySelector(".js-sample-value");
 
 const iconCheck = "c-icon_check_class";
 const isActiveField = "is-active";
 
-function toggleFields(button: HTMLElement) {
-  const selectedButton = button.getAttribute("data-target");
-  button.classList.toggle(iconCheck);
-  document.getElementById(`${selectedButton}-text`)?.classList.toggle(isActiveField);
-  document.getElementById(`${selectedButton}-select`)?.classList.toggle(isActiveField);
+let counter = 0;
+let exist = false;
+
+function buttonTrigger(trigger: HTMLElement) {
+  const selectedButton = trigger.getAttribute("data-target");
+  const inputSelect: HTMLSelectElement | null = document.querySelector(`#${selectedButton}-select`);
+  const inputText: HTMLElement | null = document.querySelector(`#${selectedButton}-text`);
+  const inputField: HTMLInputElement | null = document.querySelector(`#${selectedButton}-field`);
+
+  if (
+    selectedButton !== null &&
+    inputSelect !== null &&
+    inputText !== null &&
+    inputField !== null
+  ) {
+    if (counter === 0) {
+      counter++;
+    } else {
+      const newOption = document.createElement("option");
+      const event = new Event("change");
+      newOption.value = inputField.value;
+      newOption.textContent = inputField.value;
+      for (let i = 0; i < inputSelect.options.length; i++) {
+        if (inputSelect.options[i].value === newOption.value) {
+          exist = true;
+          break;
+        }
+      }
+
+      if (exist) {
+        exist = false;
+      } else {
+        inputSelect.appendChild(newOption);
+      }
+
+      inputSelect.value = inputField.value;
+      inputSelect.dispatchEvent(event);
+      counter--;
+    }
+
+    trigger.classList.toggle(iconCheck);
+    inputText.classList.toggle(isActiveField);
+    inputSelect.classList.toggle(isActiveField);
+  }
 }
 
-buttons.forEach(function (trigger) {
-  trigger.addEventListener("click", function () {
-    toggleFields(trigger);
+function checkFields(
+  selectElement: HTMLInputElement,
+  changeElement1: HTMLInputElement,
+  changeElement2: HTMLInputElement,
+  disabledId: string,
+) {
+  selectElement.addEventListener("change", () => {
+    if (selectElement.value.trim().length !== 0) {
+      changeElement1.disabled = false;
+      changeElement2.disabled = false;
+      document.getElementById(`${disabledId}-text`)?.classList.remove("disabled");
+    }
   });
-});
+}
+
+if (
+  buttons !== null &&
+  dataSrc !== null &&
+  dataSrcText !== null &&
+  entity !== null &&
+  entityText !== null &&
+  attribute !== null &&
+  attributeText !== null &&
+  dataType !== null &&
+  dataTypeText !== null &&
+  sampleValue !== null
+) {
+  buttons.forEach((trigger) => {
+    trigger.addEventListener("click", () => {
+      buttonTrigger(trigger);
+    });
+  });
+
+  checkFields(dataSrc, entity, entityText, "entity");
+  checkFields(entity, attribute, attributeText, "attribute");
+  checkFields(attribute, dataType, dataTypeText, "data-type");
+
+  sampleValue.addEventListener("keyup", (event: KeyboardEvent) => {
+    if (
+      event.key === "Enter" &&
+      dataSrc.value.trim().length !== 0 &&
+      entity.value.trim().length !== 0 &&
+      attribute.value.trim().length !== 0 &&
+      dataType.value.trim().length !== 0 &&
+      sampleValue.value.trim().length !== 0
+    ) {
+      parent.postMessage(
+        {
+          pluginMessage: {
+            type: "createText",
+            values: [
+              dataSrc.value,
+              entity.value,
+              attribute.value,
+              dataType.value,
+              sampleValue.value,
+            ],
+          },
+        },
+        "*",
+      );
+    }
+  });
+}
