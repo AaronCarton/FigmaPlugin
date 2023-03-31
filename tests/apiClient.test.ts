4b20970b248e5252059e48136338fc779c0d9ffa
verhaegec <91324449+verhaegec@users.noreply.github.com>
2023-03-31 13:09:59 +0200

    trying tests for archived projects

--- tests/apiClient.test.ts
+++ tests/apiClient.test.ts
@@ -1,4 +1,6 @@
 import ApiClient from "../src/services/api/client";
+import Project from "../src/interfaces/interface.project";
+import Annotation from "../src/interfaces/interface.annotation";
 import { config } from "dotenv";
 
 config();
@@ -33,7 +35,7 @@ describe("Tests for API client", () => {
       dataType: "string",
       value: "Some neat title",
     };
-    apiClient.createAnnotation("195", annotation);
+    await apiClient.createAnnotation("195", annotation);
 
     //Setup test for getProject
     const project = {
@@ -42,20 +44,56 @@ describe("Tests for API client", () => {
     };
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    apiClient.createProject("266", project).catch(() => {});
+    await apiClient.createProject("268", project).catch(() => {});
+
+    //setup test for getProject when archived
+    const projectArchived = {
+      lastUpdated: new Date().toISOString(),
+      customerId: "456",
+    };
+    (await apiClient.createProject("666", projectArchived)).archive();
+
+    //setup test for getAnnotations when archived
+    const annotationArchived = {
+      projectKey: "195",
+      nodeId: "f249d3d2",
+      dataSource: "someSource",
+      entity: "someEntity",
+      attribute: "titleArchived",
+      dataType: "string",
+      value: "Some neat title",
+    };
+    await apiClient.createAnnotation("195", annotationArchived);
   });
 
   test("Get annotations by projectKey", async () => {
-    const response = await apiClient.getAnnotations("195");
     jest.setTimeout(10000);
-    expect(response[0]).not.toBeNull();
+    const response = await apiClient.getAnnotations("195").then(async (res) => {
+      expect(res[0]).not.toBeNull();
+    });
   });
 
   test("Get Project by projectkey", async () => {
-    const response = await apiClient.getProject("266");
     jest.setTimeout(10000);
-    expect(response[0]).not.toBeNull();
+    const response = await apiClient.getProject("268").then(async (res) => {
+      expect(res[0]).not.toBeNull();
+    });
   });
+
+  test("Get archived project (by projectKey) when archived = false", async () => {
+    jest.setTimeout(10000);
+    const project = await apiClient.getProject("666", false).then(async (res) => {
+      res[0].archive();
+      const archivedProject = await apiClient.getProject("666", false);
+      expect(archivedProject[0]).not.toBeNull();
+    });
+  });
+  // test("Get annotation by projectKey when archived", async () => {
+  //   const response = await apiClient.getAnnotations("195", false);
+  //   jest.setTimeout(10000);
+  //   response[0].archive();
+  //   expect(response[0]).toBeNull();
+  // });
 });
 
 // tests: archive = false on project/annotations then try to get it back, same with archive = true
