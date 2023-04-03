2bf4b896549d78fdb44c585469c955cdf96c1771
Sebastien Vanlede <56797089+SebastienVanlede@users.noreply.github.com>
2023-04-03 11:40:16 +0200

    Add create event

--- src/services/events/eventHub.ts
+++ src/services/events/eventHub.ts
@@ -1,24 +1,28 @@
 import { EventHandler } from "./eventHandler";
 import ApiClient from "../api/client";
+import Annotation from "../../interfaces/interface.annotation";
 
 // Class with all the events
 export class EventHub {
   eventHandler = new EventHandler();
+  api = new ApiClient();
 
   // Event that initializes with the database
   init(baseURL: string, clientKey: string, sourceKey: string) {
-    this.eventHandler.makeEvent("click", (event: any) => {
+    this.eventHandler.makeEvent("click", () => {
       ApiClient.initialize({ baseURL, clientKey, sourceKey });
     });
   }
 
   // Event to create a new datasource, entity, attribute, or datatype
-  create(type: string, data: any) {
-    return null;
+  create(type: string, data: any, itemKey: string) {
+    this.eventHandler.makeEvent("click", () => {
+      this.api.upsertItem(type, itemKey, data);
+    });
   }
 
   // Event to update the annotation
   updateAnnotation(annotation: Annotation) {
-    return null;
+    this.eventHandler.makeEvent("click", () => {});
   }
 }
