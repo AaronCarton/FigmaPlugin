eed1ab6032e18fe8d89ece4b372ca7425d7167e8
Sebastien Vanlede <56797089+SebastienVanlede@users.noreply.github.com>
2023-04-05 13:07:29 +0200

    Refactor EventHub

--- src/services/events/EventHub.ts
+++ src/services/events/EventHub.ts
@@ -1,13 +1,9 @@
-import ApiClient from "../api/client";
-import Annotation from "../../interfaces/interface.annotation";
-
-// CHANGE CLASS
 // No initialization for the API, just the name of events that make an event in the event handler
-
 // Class with all the events
-export class EventHub {}
+export class EventHub {
+  // Events
+  connect = this.makeEvent("click", () => this.makeCustomEvent("connect", "connect"));
 
-export class EventHandler {
   /**
    * @description It creates an event listener
    * @param {string} eventType - The type of event to listen for
