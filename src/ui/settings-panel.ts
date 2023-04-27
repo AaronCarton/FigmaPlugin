a22103a5ee6fb6e5cc4ad3ce838474882c905ddc
SoensLars <71964506+SoensLars@users.noreply.github.com>
2023-04-27 14:55:47 +0200

    merge conflict fixes

--- src/ui/settings-panel.ts
+++ src/ui/settings-panel.ts
@@ -1,83 +1,129 @@
+import Annotation from "../interfaces/interface.annotation";
+import ApiClient from "../services/api/client";
+import EventHub from "../services/events/EventHub";
+import { Events } from "../services/events/Events";
+import { BaseComponent } from "./baseComponent";
+
+//input elements
 const $baseURL: HTMLInputElement | null = document.querySelector("#settings_dbLink");
 const $clientKey: HTMLInputElement | null = document.querySelector("#settings_clientKey");
 const $sourceKey: HTMLInputElement | null = document.querySelector("#settings_sourceKey");
+const $projectKey: HTMLInputElement | null = document.querySelector("#settings_projectKey"); // TODO: remove this when integrated with the plugin
 const $annotationToggle: HTMLInputElement | null = document.querySelector("#annotationToggle");
 const $button: HTMLButtonElement | null = document.querySelector(".c-settings__btnConnect");
+//Spinner
+const $spinner: HTMLElement | null = document.querySelector(".c-settings__loader");
+const $plugin: HTMLElement | null = document.querySelector(".js-settings-view");
 
-async function checkConnection() {
-  // Making sure there are no spaces in the values, even if the user typed spaces.
-  const baseURL: string | null | undefined = $baseURL?.value.replace(/\s/g, "").trim();
-  const clientKey: string | null | undefined = $clientKey?.value.replace(/\s/g, "").trim();
-  console.log(baseURL, clientKey);
+function initializeEventHubEvents() {
+  ApiClient.initializeEvents();
 }
 
-function toggleAnnotations(e: Event) {
-  const state: boolean = (<HTMLInputElement>e.target).checked;
-  if (state === true) {
-    console.log("Show annotations.");
-    parent.postMessage(
-      {
-        pluginMessage: {
-          type: "changeVisibility",
-          payload: { state: state },
-        },
-      },
-      "*",
-    );
-  } else {
-    console.log("Hide annotations.");
-    parent.postMessage(
-      {
-        pluginMessage: {
-          type: "changeVisibility",
-          payload: { state: state },
-        },
-      },
-      "*",
-    );
+function connect() {
+  $button?.addEventListener("click", (e: Event) => {
+    e.preventDefault();
+    EventHub.getInstance().sendCustomEvent(Events.INITIALIZE_DATA, {
+      projectKey: $projectKey?.value,
+      baseURL: $baseURL?.value,
+      clientKey: $clientKey?.value,
+      sourceKey: $sourceKey?.value,
+    });
+  });
+}
+
+export class Settings extends BaseComponent {
+  componentType = "Settings";
+
+  constructor() {
+    super();
   }
-}
 
-function disableFieldsWhenNecessary() {
-  if (
-    $baseURL !== null &&
-    $clientKey !== null &&
-    $annotationToggle !== null &&
-    $button !== null &&
-    $sourceKey !== null
-  ) {
-    // Replace makes sure people can not connect with empty strings (for example pressing spacebar).
-    if ($baseURL.value.replace(/\s/g, "") !== "") {
-      $clientKey.disabled = false;
-      $sourceKey.disabled = false;
+  initComponent(): void {
+    initializeEventHubEvents();
+    this.disableFieldsWhenNecessary();
+    this.initAnnotationToggleEvents();
+    $annotationToggle?.addEventListener("click", this.toggleAnnotations);
+    connect();
+  }
+
+  checkConnectionSpinnerExample() {
+    $plugin?.classList.add("no-pointer");
+    $spinner?.removeAttribute("hidden");
+    // const dbURL: string | null | undefined = $dbURL?.value.replace(/\s/g, "").trim();
+    // const apiKey: string | null | undefined = $apiKey?.value.replace(/\s/g, "").trim();
+    fetch("https://www.mocky.io/v2/5185415ba171ea3a00704eed?mocky-delay=5000ms")
+      .then((response) => response.json())
+      .then(() => {
+        $plugin?.classList.remove("no-pointer");
+        $spinner?.setAttribute("hidden", "");
+      });
+  }
+
+  toggleAnnotations(e: Event) {
+    const state: boolean = (<HTMLInputElement>e.target).checked;
+    if (state === true) {
+      console.log("Show annotations.");
+      parent.postMessage(
+        {
+          pluginMessage: {
+            type: "changeVisibility",
+            payload: { state: state },
+          },
+        },
+        "*",
+      );
     } else {
-      $clientKey.disabled = true;
-      $sourceKey.disabled = true;
+      console.log("Hide annotations.");
+      parent.postMessage(
+        {
+          pluginMessage: {
+            type: "changeVisibility",
+            payload: { state: state },
+          },
+        },
+        "*",
+      );
     }
+  }
+
+  disableFieldsWhenNecessary() {
     if (
-      $baseURL.value.replace(/\s/g, "") !== "" &&
-      $sourceKey.value.replace(/\s/g, "") !== "" &&
-      $clientKey.value.replace(/\s/g, "") !== ""
+      $baseURL !== null &&
+      $clientKey !== null &&
+      $annotationToggle !== null &&
+      $button !== null &&
+      $sourceKey !== null
     ) {
-      $annotationToggle.disabled = false;
-      $button.addEventListener("click", checkConnection);
+      //replace makes sure people can not connect with empty strings (for example pressing spacebar)
+      if ($baseURL.value.replace(/\s/g, "") !== "") {
+        $clientKey.disabled = false;
+      } else {
+        $clientKey.disabled = true;
+      }
+      if ($baseURL.value.replace(/\s/g, "") !== "" && $clientKey.value.replace(/\s/g, "") !== "") {
+        $sourceKey.disabled = false;
+      } else {
+        $sourceKey.disabled = true;
+      }
+      if (
+        $baseURL.value.replace(/\s/g, "") !== "" &&
+        $sourceKey.value.replace(/\s/g, "") !== "" &&
+        $clientKey.value.replace(/\s/g, "") !== ""
+      ) {
+        console.log("enable button");
+        $annotationToggle.disabled = false;
+        $button.addEventListener("click", this.checkConnectionSpinnerExample);
+      }
     } else {
       $annotationToggle.disabled = true;
-      $button.removeEventListener("click", checkConnection);
+      $button.removeEventListener("click", this.checkConnectionSpinnerExample);
     }
   }
-}
 
-function initAnnotationToggleEvents() {
-  $annotationToggle?.addEventListener("click", toggleAnnotations);
-  $baseURL?.addEventListener("keyup", disableFieldsWhenNecessary);
-  $clientKey?.addEventListener("keyup", disableFieldsWhenNecessary);
-  $sourceKey?.addEventListener("keyup", disableFieldsWhenNecessary);
+  initAnnotationToggleEvents() {
+    $annotationToggle?.addEventListener("click", this.toggleAnnotations);
+    $baseURL?.addEventListener("keyup", this.disableFieldsWhenNecessary);
+    $clientKey?.addEventListener("keyup", this.disableFieldsWhenNecessary);
+    $sourceKey?.addEventListener("keyup", this.disableFieldsWhenNecessary);
+  }
 }
-
-function init() {
-  disableFieldsWhenNecessary();
-  initAnnotationToggleEvents();
-}
-
-init();
