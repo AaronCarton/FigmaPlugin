4673c149254a80247c208419d5dd552eaf9968f0
SoensLars <71964506+SoensLars@users.noreply.github.com>
2023-05-30 16:25:24 +0200

    Made filtering on more than one property possible

--- src/ui/filter-panel.ts
+++ src/ui/filter-panel.ts
@@ -20,9 +20,6 @@ export class FilterPanel extends BaseComponent {
 function initializeFilterFunction() {
   if ($resetFilterButton && $filterButton && $filterSelectors && $isFilterActive) {
     $filterButton.addEventListener("click", () => {
-      $filterSelectors.forEach((selector) => {
-        showFiltersConnectPanel(selector);
-      });
       $isFilterActive.style.opacity = "1";
       $resetFilterButton.disabled = false;
       $filterButton.disabled = true;
@@ -40,35 +37,11 @@ function initializeFilterFunction() {
 
     $filterSelectors.forEach((selector) => {
       selector.addEventListener("change", () => {
-        if (selector.value !== "") {
-          $filterSelectors.forEach((selector) => {
-            selector.disabled = true;
-            $filterButton.disabled = true;
-          });
+        $filterSelectors.forEach((selector) => {
           selector.disabled = false;
           $filterButton.disabled = false;
-        } else {
-          $filterSelectors.forEach((selector) => {
-            selector.disabled = false;
-            $filterButton.disabled = false;
-          });
-        }
+        });
       });
     });
-  }
-}
-
-function showFiltersConnectPanel(input: HTMLInputElement) {
-  const target = input.getAttribute("data-target");
-  const targetItem = document.querySelector<HTMLElement>(`#${target}-item`);
-  const targetText = document.querySelector<HTMLElement>(`#${target}-text`);
-  if (input.value !== "") {
-    if (targetItem && targetText) {
-      targetItem.classList.add("is-active");
-      targetText.innerHTML = input.value;
-    }
-  } else if (targetItem && targetText) {
-    targetItem.classList.remove("is-active");
-    targetText.innerHTML = "";
   }
 }
