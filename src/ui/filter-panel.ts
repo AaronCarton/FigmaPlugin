4a10eefff3fe828de61e97ff4429746ef408e2e4
SoensLars <71964506+SoensLars@users.noreply.github.com>
2023-06-02 14:31:55 +0200

    Added correct filter-page into this branch

--- src/ui/filter-panel.ts
+++ src/ui/filter-panel.ts
@@ -1,13 +1,9 @@
-import EventHub from "../services/events/EventHub";
-import { Events } from "../services/events/Events";
 import { BaseComponent } from "./baseComponent";
 
 const $resetFilterButton: HTMLButtonElement | null = document.querySelector(".js-reset-btn");
 const $filterButton: HTMLButtonElement | null = document.querySelector(".js-filter-btn");
-const $filterItems: NodeListOf<HTMLElement> | null = document.querySelectorAll(".js-filter-item");
-const $removeFilter: NodeListOf<HTMLElement> | null = document.querySelectorAll(".js-remove-filter");
 const $filterSelectors: NodeListOf<HTMLInputElement> | null = document.querySelectorAll(".js-filter");
-let count: number = 0;
+const $isFilterActive: HTMLElement | null = document.querySelector(".js-filter-active");
 
 export class FilterPanel extends BaseComponent {
   componentType = "FilterPanel";
@@ -22,84 +18,30 @@ export class FilterPanel extends BaseComponent {
 }
 
 function initializeFilterFunction() {
-  if ($resetFilterButton && $filterButton && $filterSelectors) {
+  if ($resetFilterButton && $filterButton && $filterSelectors && $isFilterActive) {
     $filterButton.addEventListener("click", () => {
-      count = 0;
-      $filterSelectors.forEach((selector) => {
-        showFiltersConnectPanel(selector);
-      });
-      if ($filterItems) {
-        $filterItems.forEach((item) => {
-          if (item.classList.contains("is-active")) {
-            count++;
-          }
-        });
-        EventHub.getInstance().sendCustomEvent(Events.UI_CHANGE_FILTER, count);
-      }
+      $isFilterActive.style.opacity = "1";
+      $resetFilterButton.disabled = false;
+      $filterButton.disabled = true;
     });
 
     $resetFilterButton.addEventListener("click", () => {
-      count = 0;
       $filterSelectors.forEach((selector) => {
         selector.value = "";
         selector.disabled = false;
       });
-      $filterItems?.forEach((item) => {
-        item.classList.remove("is-active");
-      });
-      EventHub.getInstance().sendCustomEvent(Events.UI_CHANGE_FILTER, count);
+      $isFilterActive.style.opacity = "0";
+      $resetFilterButton.disabled = true;
+      $filterButton.disabled = true;
     });
 
     $filterSelectors.forEach((selector) => {
       selector.addEventListener("change", () => {
-        if (selector.value !== "") {
-          $filterSelectors.forEach((selector) => {
-            selector.disabled = true;
-          });
+        $filterSelectors.forEach((selector) => {
           selector.disabled = false;
-        } else {
-          $filterSelectors.forEach((selector) => {
-            selector.disabled = false;
-          });
-        }
+          $filterButton.disabled = false;
+        });
       });
     });
-
-    removeFilterItem();
   }
-}
-
-function showFiltersConnectPanel(input: HTMLInputElement) {
-  const target = input.getAttribute("data-target");
-  const targetItem = document.querySelector<HTMLElement>(`#${target}-item`);
-  const targetText = document.querySelector<HTMLElement>(`#${target}-text`);
-  if (input.value !== "") {
-    if (targetItem && targetText) {
-      targetItem.classList.add("is-active");
-      targetText.innerHTML = input.value;
-    }
-  } else if (targetItem && targetText) {
-    targetItem.classList.remove("is-active");
-    targetText.innerHTML = "";
-  }
-}
-
-function removeFilterItem() {
-  $removeFilter?.forEach((item) => {
-    item.addEventListener("click", () => {
-      const target = item.getAttribute("data-target");
-      const targetItem = document.querySelector<HTMLElement>(`#${target}`);
-      targetItem?.classList.remove("is-active");
-      count--;
-      EventHub.getInstance().sendCustomEvent(Events.UI_REMOVE_FILTER, count);
-      unselectRemoved();
-    });
-  });
-}
-
-function unselectRemoved() {
-  $filterSelectors?.forEach((selector) => {
-    selector.disabled = false;
-    selector.value = "";
-  });
 }
